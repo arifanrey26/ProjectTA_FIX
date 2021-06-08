791299c62e79ed4de8ce62c7c828bbeceaa39b11
arifanrey26 <58202185+arifanrey26@users.noreply.github.com>
2021-06-07 20:56:33 -0800

    8

--- app/src/main/java/com/example/projectta/DiagnosaActivity.java
+++ app/src/main/java/com/example/projectta/DiagnosaActivity.java
@@ -17,7 +17,17 @@ public class DiagnosaActivity extends AppCompatActivity {
             Check4,
             Check5,
             Check6,
-            Check7;
+            Check7,
+            Check8,
+            Check9,
+            Check10,
+            Check11,
+            Check12,
+            Check13,
+            Check14,
+            Check15,
+            Check16,
+            Check17;
 
     Button btn_klik_diagnosa;
     TextView tv_letak_kerusakan;
@@ -36,6 +46,16 @@ public class DiagnosaActivity extends AppCompatActivity {
         Check5 = (CheckBox) findViewById(R.id.checkBox5);
         Check6 = (CheckBox) findViewById(R.id.checkBox6);
         Check7 = (CheckBox) findViewById(R.id.checkBox7);
+        Check8 = (CheckBox) findViewById(R.id.checkBox8);
+        Check9 = (CheckBox) findViewById(R.id.checkBox9);
+        Check10 = (CheckBox) findViewById(R.id.checkBox10);
+        Check11 = (CheckBox) findViewById(R.id.checkBox11);
+        Check12 = (CheckBox) findViewById(R.id.checkBox12);
+        Check13 = (CheckBox) findViewById(R.id.checkBox13);
+        Check14 = (CheckBox) findViewById(R.id.checkBox14);
+        Check15 = (CheckBox) findViewById(R.id.checkBox15);
+        Check16 = (CheckBox) findViewById(R.id.checkBox16);
+        Check17 = (CheckBox) findViewById(R.id.checkBox17);
 
         btn_klik_diagnosa = (Button) findViewById(R.id.btn_klikdiagnosa);
         tv_letak_kerusakan = (TextView) findViewById(R.id.tv_letakkerusakan);
@@ -51,13 +71,32 @@ public class DiagnosaActivity extends AppCompatActivity {
                 String SolusiKerusakan = "Solusinya yaitu : ";
 
                 //Output Kerusakan : a
-                if (Check1.isChecked() && Check2.isChecked() && Check3.isChecked()){
+                if (Check1.isChecked() && Check2.isChecked() && Check3.isChecked() && Check4.isChecked()){
                     LetakKerusakan+= "\nBUSI";
                     SolusiKerusakan+= "\nMengganti Busi Dengan Yang Baru";
-
                 }
-                if (Check4.isChecked() && Check5.isChecked() && Check6.isChecked() && Check7.isChecked()){
+                if (Check5.isChecked() && Check6.isChecked() && Check7.isChecked() && Check8.isChecked()){
+                    LetakKerusakan+= "\nAcu";
+                    SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
+                }
+                if (Check2.isChecked() && Check9.isChecked()){
+                    LetakKerusakan+= "\nCelah Klep";
+                    SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
+                }
+                if (Check5.isChecked() && Check1.isChecked() && Check3.isChecked() && Check10.isChecked()){
+                    LetakKerusakan+= "\nInjector";
+                    SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
+                }
+                if (Check1.isChecked() && Check3.isChecked() && Check11.isChecked() && Check12.isChecked() && Check13.isChecked()){
+                    LetakKerusakan+= "\nRoller";
+                    SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
+                }
+                if (Check3.isChecked() && Check15.isChecked()){
                     LetakKerusakan+= "\nCVT";
+                    SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
+                }
+                if (Check16.isChecked() && Check17.isChecked()){
+                    LetakKerusakan+= "\nRoller";
                     SolusiKerusakan+= "\nPerbaiki Pada Mekanik/Ahlinya";
                 }
 
@@ -93,6 +132,36 @@ public class DiagnosaActivity extends AppCompatActivity {
                     break;
                 case R.id.checkBox7:
                     string = checked?"Gejala 7 Diseleksi":"Gejala 7 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox8:
+                    string = checked?"Gejala 8 Diseleksi":"Gejala 8 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox9:
+                    string = checked?"Gejala 9 Diseleksi":"Gejala 9 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox10:
+                    string = checked?"Gejala 10 Diseleksi":"Gejala 10 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox11:
+                    string = checked?"Gejala 11 Diseleksi":"Gejala 11 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox12:
+                    string = checked?"Gejala 12 Diseleksi":"Gejala 12 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox13:
+                    string = checked?"Gejala 13 Diseleksi":"Gejala 13 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox14:
+                    string = checked?"Gejala 14 Diseleksi":"Gejala 14 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox15:
+                    string = checked?"Gejala 15 Diseleksi":"Gejala 15 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox16:
+                    string = checked?"Gejala 16 Diseleksi":"Gejala 16 Tidak Diseleksi";
+                    break;
+                case R.id.checkBox17:
+                    string = checked?"Gejala 17 Diseleksi":"Gejala 17 Tidak Diseleksi";
                     break;
             }
 
