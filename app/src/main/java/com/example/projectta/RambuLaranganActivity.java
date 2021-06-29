ecf0925fd6939c572dbc0c8201fb43ba451f2fbe
arifanrey26 <58202185+arifanrey26@users.noreply.github.com>
2021-06-29 08:15:24 -0800

    13

---
+++ app/src/main/java/com/example/projectta/RambuLaranganActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RambuLaranganActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rambu_larangan);
+    }
+}
---
+++ app/src/main/java/com/example/projectta/RambuPeringatanActivity.java
@@ -0,0 +1,14 @@
+package com.example.projectta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RambuPeringatanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rambu_peringatan);
+    }
+}
