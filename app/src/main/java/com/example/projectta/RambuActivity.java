b7f85b634d71f3e7b3a7c0c8f1a229e180cc0a11
arifanrey26 <58202185+arifanrey26@users.noreply.github.com>
2021-06-29 21:52:45 -0800

    14

--- app/src/main/java/com/example/projectta/RambuActivity.java
+++ app/src/main/java/com/example/projectta/RambuActivity.java
@@ -11,15 +11,49 @@ import com.github.barteksc.pdfviewer.PDFView;
 
 public class RambuActivity extends AppCompatActivity {
 
-    PDFView pdf_rambu;
+    private Button btn_rmblrg, btn_rmbprgtn, btn_rmbptnj, btn_rmbprt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rambu);
 
-        pdf_rambu = (PDFView) findViewById(R.id.pdfRambu);
+        btn_rmblrg = findViewById(R.id.btn_jenislrg);
+        btn_rmbprgtn = findViewById(R.id.btn_jenisprgtn);
+        btn_rmbptnj = findViewById(R.id.btn_jenisptnj);
+        btn_rmbprt = findViewById(R.id.btn_jenisprt);
 
-        pdf_rambu.fromAsset("rambu-rambu.pdf").load();
+        btn_rmblrg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RambuActivity.this, RambuLaranganActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_rmbprgtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RambuActivity.this, RambuPeringatanActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_rmbptnj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RambuActivity.this, RambuPetunjukActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        btn_rmbprt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RambuActivity.this, RambuPerintahActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
